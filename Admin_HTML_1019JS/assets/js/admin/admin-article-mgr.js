1fd836edf4b5d1e479f692a845206a55c77a7920
yangjianibn <851110276@qq.com>
2016-10-19 21:31:10 +0800

    add

--- Admin_HTML_1019JS/assets/js/admin/admin-article-mgr.js
+++ Admin_HTML_1019JS/assets/js/admin/admin-article-mgr.js
@@ -1,4 +1,18 @@
 $(function(){
+
+    //init
+    $('.field_date').datetimepicker({
+        language: 'zh-CN',
+        format: 'yyyy-mm-dd',
+        autoclose: true,
+        weekStart: 1,
+        minView: 2
+    });
+
+    query();
+
+
+
     $('#delete').click(function(){
         $.dialog({
             title:'温馨提示',
@@ -11,3 +25,44 @@ $(function(){
         })
     });
 })
+
+
+function query() {
+    var _status = $('#search-box .status ul > li.active').attr('data-id');
+    $("#list").hiMallDatagrid({
+        url: 'yours_url',
+        nowrap: false,
+        rownumbers: true,
+        NoDataMsg: '没有找到符合条件的数据',
+        border: false,
+        fit: true,
+        fitColumns: true,
+        pagination: true,
+        idField: "Id",
+        pageSize: 15,
+        pageNumberName:"page",
+        pageSizeName: "size",
+        backDataName:"data",
+        totalName:"totalNum",
+        pagePosition: 'bottom',
+        pageNumber: 1,
+        queryParams: { },
+        columns:[[
+            { field: "inquirySheetCode", title: "询价编号", width: 80, align: "center" },
+            { field: "title", title: "工程名称", width: 80, align: "center" },
+            { field: "publishDate", title: "提交日期", width: 80, align: "center" },
+            { field: "quotationEndDate", title: "询价截止日期", width: 80, align: "center" },
+            { field: "stateStr", title: "状态", width: 80, align: "center"},
+            { field: "operation", operation: true, title: "操作", width: 80,
+            	formatter: function (value, row, index) {
+	                var code = row.inquirySheetCode.toString();
+	                var html = ["<span class=\"btn-a\" > "];
+	                html.push("<a href='javascript:modifyInquiry(\"" + code + "\")'>修改</a>");
+	                html.push("<a href='javascript:closeInquiry(\"" + code + "\")'>关闭</a>");
+	                html.push("</span>");
+	                return html.join("");
+	            }
+            }
+        ]]
+    });
+}
